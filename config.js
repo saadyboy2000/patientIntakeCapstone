e8dada0db16cc6ae019ab5762b23185184147eb6
saaderana <saaderana@gmail.com>
2017-11-08 14:23:34 -0800

    added mongodb with user

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/formDb';
+                      'mongodb://user:user@ds155315.mlab.com:55315/formdb';
 exports.PORT = process.env.PORT || 8080;
 
 //have all collections for every schema in one database
