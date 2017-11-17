f76789afc0e7d719fc01578e1bf03d1164ca37d0
saaderana <saaderana@gmail.com>
2017-11-17 14:48:56 -0800

    downgrade delete2

--- public/client.js
+++ public/client.js
@@ -315,8 +315,6 @@ function handleDisplayForms() {
 function handleDeleteForms() {
     $('#form-container').on('click', '#delete-button', function () {
         deleteForm($(this).attr("data-form-id"));
-        let itemElement = $(this).attr("data-form-id");
-        itemElement.remove();
     });
 }
 
