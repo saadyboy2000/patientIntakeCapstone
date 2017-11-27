786d2161334017dac6f0ab2501ebf798099203b8
saaderana <saaderana@gmail.com>
2017-11-27 11:11:50 -0800

    deleted space

--- public/client.js
+++ public/client.js
@@ -154,27 +154,7 @@ function displayFormById() {
                                 <button id="delete-button" class="nav-button">Delete</button>
                                 <button id="nav-view-button" class="view-button">View All</button>`;
 
-               /* htmlOutput += '<div class="current-form">';
-                htmlOutput += '<input type="hidden" class="formID" value="';
-                htmlOutput += data._id;
-                htmlOutput += '">';
-                htmlOutput += '<h2>Form for: </h2>';
-                htmlOutput += '<p class="form-doctor">';
-                htmlOutput += data.doctor;
-                htmlOutput += '</p><br><br>';
-                htmlOutput += '<h2>Specialty: </h2>';
-                htmlOutput += '<p class="form-specialty">';
-                htmlOutput += data.specialty;
-                htmlOutput += '</p><br><br>';
-                htmlOutput += '<h2>Questions: </h2>';
-                htmlOutput += '<p class="form-question">';
-                htmlOutput += data.questions;
-                htmlOutput += '</p><br><br>';
-                htmlOutput += '</div>';
-                htmlOutput += '<button id="edit-button" class="form-button">Edit</button>';
-                htmlOutput += '<button id="delete-button" class="form-button">Delete</button>';
-                htmlOutput += '<button id="nav-view-button" class="form-button">View All</button>'; */
-
+              
                 $('#forms').html(htmlOutput);
                 displayForms();
             })
@@ -212,7 +192,7 @@ function retrieveForm() {
                  <label>Max age intended:</label><br>
                 <input type = "number" name="age" id="age" value="${data.id}"><br>
                 <label>Questions:</label><br>
-                <textarea id = "questions" name = "questions" required value = "${data.questions}"></textarea><br>
+                <textarea id = "questions" name = "questions" required value="${data.questions}"></textarea><br>
                 <button type="submit" id="update-button">Update</button>
                 </fieldset>
                 </form>`)
