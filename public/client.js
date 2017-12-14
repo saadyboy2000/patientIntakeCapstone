fc7dc7dc5207e8fc69cfcc529f1039978bc431e4
saaderana <saaderana@gmail.com>
2017-12-14 14:30:14 -0800

    accounted for form-container id

--- public/client.js
+++ public/client.js
@@ -79,7 +79,7 @@ function postNewForm() {
 //display all forms
 function displayForms() {
     $('.view-button').on('click', function () {
-        $('.form-container').removeClass("hide");
+        $('#form-container').removeClass("hide");
         $('#createForm').addClass("hide");
     $.ajax({
             method: 'GET',
@@ -297,7 +297,7 @@ function navCreateButton() {
     $('.dropdown-content').on('click', '#nav-create-button', function () {
         console.log('clicked');
          $('#createForm').removeClass("hide");
-         $('form-container').addClass("hide");
+         $('#form-container').addClass("hide");
          $('#new-form').html(`<fieldset>
             <legend class="section-header">Create a form</legend>
             <label>Doctor:</label><br>
